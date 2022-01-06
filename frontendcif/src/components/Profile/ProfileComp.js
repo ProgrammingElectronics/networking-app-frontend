7a1567ac0af3a6715f652a5b8479fc8ca830f5b4
Kristen Ruprecht <ksruprec@gmail.com>
2022-01-06 16:47:13 -0500

    added connections and profile container components

---
+++ frontendcif/src/components/Profile/ProfileComp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfileComp = () => {
+    return (
+        <div>
+            <h1>Display Profile here</h1>
+            <h2>This is the actual profile info displayed</h2>
+        </div>
+    )
+}
+
+export default ProfileComp
---
+++ frontendcif/src/components/ProfileContainer/ProfileContainerComp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//components
+import ProfileComp from '../Profile/ProfileComp'
+//styles
+import "./ProfileContainerStyles.css"
+
+const ProfileContainerComp = () => {
+    return (
+        <div className="profileContainer">
+            <ProfileComp/>
+        </div>
+    )
+}
+
+export default ProfileContainerComp
