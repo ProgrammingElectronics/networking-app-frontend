64ee5e3b5159271d119596794119cd97231931ea
Zachary Blackburn <>
2022-01-18 14:31:54 -0500

    Can no longer send messages to pending

--- frontendcif/src/components/ConnectionMiniCard/PendingMiniCardComp.js
+++ frontendcif/src/components/ConnectionMiniCard/PendingMiniCardComp.js
@@ -1,50 +1,102 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import { MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
-
+import ProfileAPI from '../../api/ProfileAPI'
 //styles
 import "./MiniCardStyles.css"
+import ProfileDetailsModalComp from '../ModalComps/ProfileDetailsModalComp';
 
 const PendingMiniCardComp = (props) => {
+    
+    const [toProfile, setToProfile] = useState(null)
+    
+
     // props
     const { connection } = props
-    // card variables
-    let imageURL = connection['from_profile']['img_url']
-    let firstName = connection['from_profile']['user']['first_name']
-    let lastName = connection['from_profile']['user']['last_name']
-    let role = connection['from_profile']['is_professional']
-    let bootcamp = connection['from_profile']['enrollment'][0]['bootcamp']['name']
+    
+    // console.log('minicardcomp | connection', connection)
+
+    //helper variables
+    let toProfileID = connection['to_profile']
+    
+    let token = localStorage.getItem("auth-user")
+    const userProfileID = localStorage.getItem('user_profile')
+
+    
+    useEffect(() => {
+        const getToProfile = async () => {
+            if (connection) {
+                let data = await  ProfileAPI.getProfileByID(token, toProfileID);
+                
+                setToProfile(data)
+                
+            }
+        }   
+        getToProfile()
+    }, [])
+
+    //card variables
     let status = connection['status']
 
-    //this card is not needed, logic is already set in ConnectionsContainerComp to separate pending vs active connections
-
-    return (
-        <div className="miniCard">
-              <MDBCard className="mdb-minicard" style={{ maxWidth: '540px' }}>
+    const renderMiniCard = () => {
+       
+        // const bootcampName = toProfile['enrollment'][0]['bootcamp']['name'];
+        // const graduationYear = toProfile['enrollment'][0]['graduation_year'];
+        // const isPro = toProfile['is_professional']
+        // ######################################################################################
+        // This case displays the connection where the user was requested.  
+        // So it should show the FROM_PROFILE info
+        // ###################################################################################
+        
+        return (
+            <div>
+              <MDBCard className="mdb-minicard">
                 <MDBRow className='g-0'>
-                    <MDBCol md='4'>
-                    <MDBCardImage classname="minicard-pic" style={{ maxHeight: '148px'}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo-CP7ChC_RjSlZcIC59_FH4KBl41Enn74Zw&usqp=CAU' alt='...' fluid />
+                    <MDBCol className='img-col' md='3'>
+                        <MDBCardImage className="minicard-pic" src={toProfile['img_url']} alt='profile picture' fluid='true' />
                     </MDBCol>
-                    <MDBCol md='6'>
-                    <MDBCardBody>
-                        <MDBCardTitle>{firstName} {lastName}</MDBCardTitle>
-                        <MDBCardText>
-                        Professional | {bootcamp}
-                        </MDBCardText>
-                        <MDBCardText>
-                            <small>Graduated 2019</small>
-                        </MDBCardText>
-                        <MDBCardText>
-                            <small>Graduated 2019</small>
-                        </MDBCardText>
-                    </MDBCardBody>
-                    </MDBCol>
-                    <MDBCol className="btn-col">
-                        <MDBBtn disabled class="btn btn-info btn-sm">Pending</MDBBtn>
-                    </MDBCol>
+
+                    <MDBCol >
+                    
+                        <MDBCardBody>
+                            <MDBCardTitle>{toProfile['user']['first_name']} {toProfile['user']['last_name']}</MDBCardTitle>
+                            <MDBCol>
+                            {toProfile['enrollment'][0] ?
+                            <>
+                            <MDBCardText>{toProfile['enrollment'][0]['bootcamp']['name']} <small>{toProfile['enrollment'][0]['graduation_year']}</small></MDBCardText>
+                            </>
+                            :
+                            <>
+                            {null}
+                            </>
+                            }                                
+                            </MDBCol>
+                            <MDBCardText>
+                                <small>Status: {status}</small>
+                            </MDBCardText>
+                            <MDBRow>
+                                <MDBCol className="btn-col">
+                                    <ProfileDetailsModalComp profile={toProfile}/>
+                                </MDBCol>
+                            </MDBRow>
+                        </MDBCardBody>                   
+                    </MDBCol>                               
                 </MDBRow>
               </MDBCard>
+            </div>
+        );
+    }
+
+
+    
+    
+         return (
+            
+        <div className="miniCard">
+            {toProfile && renderMiniCard()}
         </div>
+             
     )
+         
 }
 
 export default PendingMiniCardComp
